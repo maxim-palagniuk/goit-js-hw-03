1cf25606535fd32772f4d42d2f0549433ed0cc00
b1zecs <maxlolkill@gmail.com>
2026-07-31 00:20:51 +0300

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function makeArray(firstArray, secondArray, maxLength) {
     const concatArray = firstArray.concat(secondArray);
     const resArray = []
-    for (let i = 0; i < maxLength; i++) {
+    for (let i = 0; i < maxLength && i < concatArray.length; i++) {
         resArray.push(concatArray[i]);
     }
         
@@ -16,4 +16,4 @@ console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); //
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); //
